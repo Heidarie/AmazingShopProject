a6eb1a938ee217d1f25a16a378b6a83cbf6d5fd2
Heidarie <68645579+Heidarie@users.noreply.github.com>
2022-01-16 00:18:10 +0100

    Update DatabaseEntities.cs

--- server/PlaczekProj/Entities/DatabaseEntities.cs
+++ server/PlaczekProj/Entities/DatabaseEntities.cs
@@ -27,7 +27,7 @@ namespace PlaczekProj.Entities
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=127.0.0.1;port=3306;user=root;password=Haslo.To.Dawcio135;database=placzekproj", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.27-mysql"));
+                optionsBuilder.UseMySql("server=127.0.0.1;port=3306;user=root;password=;database=", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.27-mysql"));
             }
         }
 
